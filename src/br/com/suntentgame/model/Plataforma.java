55ac2450409ceae46c19f98cad8e6123bad7a334
cesarledres <cesarledrescv@gmail.com>
2026-09-07 14:24:05 -0300

    refactor: refatora para deixar classe mais aderente a regra de negócio, remove elementos e espaços desnecessários da classe Plataforma.

--- src/br/com/suntentgame/model/Plataforma.java
+++ src/br/com/suntentgame/model/Plataforma.java
@@ -4,36 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Plataforma {
-
-	//atributos
-
 	private List<Usuario> usuarios;
 	private List<Video> videos;
-
-
-	//construtor
 
 	public Plataforma() {
 		usuarios = new ArrayList<>();
 		videos = new ArrayList<>();
 	}
 
-	//get
-
 	public List<Usuario> getUsuarios() {
 		return usuarios;
 	}
 
-	public List<Video> getVideos() {
-		return videos;
-	}
-
-	//metodos
-
-	public boolean cadastrarUsuario(Usuario usuario) {
+	public void cadastrarUsuario(Usuario usuario) {
 		usuarios.add(usuario);
 		System.out.println("Usuário cadastrado com sucesso! ID do usuário: " + usuario.getIdUsuario());
-		return true;
 	}
 
 	public Usuario buscarUsuarioPorId(int idUsuario) {
@@ -45,10 +30,9 @@ public class Plataforma {
 		return null;
 	}
 
-	public boolean cadastrarVideo(Video video) {
+	public void cadastrarVideo(Video video) {
 		videos.add(video);
 		System.out.println("Vídeo cadastrado com sucesso! ID do vídeo: " + video.getIdVideo());
-		return true;
 	}
 
 	public Video buscarVideoPorId(int idVideo) {
